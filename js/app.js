1f8f20ec1d6e128eac625e3689ad097408b721db
Maciej Preus <mpreus@onet.eu>
2019-06-13 22:45:06 +0200

    Poprawienie funkcjonalności wersji mobilnej i refactoring

--- js/app.js
+++ js/app.js
@@ -12,101 +12,116 @@ element 'p' ma właściwość 'display', którą metoda 'toggle()' żongluje war
 	var aboutMeModule = $("h3.myStory");
 	aboutMeModule.on("click", function() {
 		$(this).next().toggle("slow")
-	});
-	var beginnings = $("h3#beginnings");
-	beginnings.on("click", function() {
-		$("div.pictureAdditional").toggle("slow")
-	});
-	
+	});	
 
 //sprawdź przy ładowaniu strony rozmiar okna przeglądarki...
 	var windowWidth = $(window).width();
 	if (windowWidth < 920) { // ...i dostosuj sposób wyświetlania
+		aboutMeModule.next().css("display", "none");
 		$(".twoSections").css("display", "block");
-		$(".section2").css(
+		
+		$(".section2, .section3, .section4, .section5").css(
 		{
 			"display": "block",
 			"width": "90%",
 			"margin": "0 auto"
 		});
-		$(".section3").css(
+		
+		$(".jumbotron").css(
 		{
-			"display": "block",
-			"width": "90%",
-			"margin": "0 auto"
+			"padding-top": "4vw",
+			"margin-bottom": "2vw"
 		});
-		$(".section4").css(
-		{
-			"display": "block",
-			"width": "90%",
-			"margin": "0 auto"
-		});
-		$(".section5").css(
-		{
-			"display": "block",
-			"width": "90%",
-			"margin": "0 auto"
-		});
+		
 		$("h2").css(
 		{
 			"fontSize": "6vw" // dodatkowo - wcześniej nie było
 		});
+		
 		$("h3").css(
 		{
-			"fontSize": "5vw" // dodatkowo - wcześniej nie było
+			"fontSize": "5vw", // dodatkowo - wcześniej nie było
+			"margin-top": "2vw"
 		});
+		
+		$("#interests1 h3, #interests2 h3").css(
+		{
+			"padding-top": "2vw"
+		});
+		
 		$(".aboutMe, .my-works, .my-interests").css(
 		{
-			"fontSize": "3.5vw", // było 2.5 - za mało!
+			"fontSize": "4vw", // było 2.5 - za mało!
 			"text-align": "left"
 		});
+		
 		$("h4").css( //.techno
 		{
 			"fontSize": "4vw", // było 3.5 - za mało!
 			"textAlign": "center"
 		});
+		
 		$(".techno p").css(
 		{
-			"fontSize": "3.5vw" // było 2.5 - za mało!
+			"fontSize": "4vw" // było 2.5 - za mało!
 		});
+		
 		$(".my-works img").css(
 		{
-			"width": "45vw",
+			"width": "40vw",
 			"height": "24vw"
 		});
-		$(".aboutMe img#gitHub").css(
+		
+		$(".aboutMe img.gitHub").css(
 		{
 			"width": "45vw",
 			"height": "36vw"
 		});
+		
 		$(".my-interests img").css(
 		{
-			"width": "45vw",
+			"width": "40vw",
 			"height": "24vw"
 		});
-		$(".pictureAdditional img").css(
+		
+		$(".obrazek").css(
 		{
-			"width": "90vw",
-			"height": "75vw",
-			"margin": "0 auto"
+			"text-align": "center"
 		});
-		$("#obrazek img").css(
+		
+		$(".obrazek img").css(
 		{
-			"width": "40vw"
+			"width": "42vw"
 		});
+		
 		$(".photo p").css(
 		{
-			"fontSize": "3.5vw" // było 2.5 - za mało!
-		})
+			"fontSize": "3.5vw", // było 2.5 - za mało!
+			"margin-bottom": "1vw"
+		});
+		
+		$(".photo p.my-pictures").css(
+		{
+			"fontSize": "3.5vw", // było 2.5 - za mało!
+			"padding-bottom": "2vw"
+		});
+		
 		$("a").css(
 		{
-			"fontSize": "4vw" // było 2 - za mało!
+			"fontSize": "3.5vw" // było 2 - za mało!
 		});
-		$(".littleTitle").css(
+		
+		$(".littleTitle p").css(
 		{
-			"lineHeight": "2vw"
+			"font-size": "3vw",
+			"lineHeight": "4vw"
 		});
-	};
-		 	
+		
+		$("footer p, footer a").css(
+		{
+			"fontSize": "2vw",
+			"lineHeight": "2.5vw"
+		});
+	};	 	
 });
 
